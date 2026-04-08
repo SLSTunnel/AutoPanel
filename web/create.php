cb09838cfad50f315b4efb638da3f5b964e15633
SLSTunnel <106473683+SLSTunnel@users.noreply.github.com>
2026-04-08 13:54:16 -0500

    Enhance user creation with password hashing and expiry

--- web/create.php
+++ web/create.php
@@ -1,10 +1,25 @@
 <?php
+session_start();
+if(!$_SESSION['ok']) header("Location:login.php");
+
 if($_POST){
-file_put_contents("/etc/autopanel/data/users.db",$_POST['user']."\n",FILE_APPEND);
-echo "Created";
+$user=$_POST['user'];
+$pass=$_POST['pass'];
+$days=$_POST['days'];
+
+$exp=date("Y-m-d",strtotime("+$days days"));
+$hash=hash("sha256",$pass);
+
+file_put_contents("/etc/autopanel/data/users.db","$user:$hash\n",FILE_APPEND);
+file_put_contents("/etc/autopanel/data/expiry.db","$user $exp\n",FILE_APPEND);
+
+echo "Created!";
 }
 ?>
+
 <form method="post">
-User:<input name="user">
+User:<input name="user"><br>
+Pass:<input name="pass"><br>
+Days:<input name="days"><br>
 <button>Create</button>
 </form>
