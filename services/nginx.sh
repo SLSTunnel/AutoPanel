4b41b3f6d430e133133e20dc3abe7e00fa7728fa
SLSTunnel <106473683+SLSTunnel@users.noreply.github.com>
2026-04-08 14:14:04 -0500

    Update nginx.sh

--- services/nginx.sh
+++ services/nginx.sh
@@ -1,3 +1,29 @@
 #!/bin/bash
-systemctl enable nginx
-systemctl start nginx
+
+echo "[KING] Configuring Nginx..."
+
+cat > /etc/nginx/sites-enabled/default <<EOF
+server {
+    listen 8080 default_server;
+    server_name _;
+
+    location / {
+        root /var/www/html;
+        index index.php index.html;
+    }
+
+    location /ws {
+        proxy_pass http://127.0.0.1:3000;
+        proxy_http_version 1.1;
+        proxy_set_header Upgrade \$http_upgrade;
+        proxy_set_header Connection "Upgrade";
+    }
+
+    location ~ \.php$ {
+        include snippets/fastcgi-php.conf;
+        fastcgi_pass unix:/run/php/php-fpm.sock;
+    }
+}
+EOF
+
+systemctl restart nginx
