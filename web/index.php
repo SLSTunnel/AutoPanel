97618c04496ead4ae8b6f20a1839cb741493abf0
SLSTunnel <106473683+SLSTunnel@users.noreply.github.com>
2026-04-08 13:54:54 -0500

    Add crown emoji to DevSupport Panel header

--- web/index.php
+++ web/index.php
@@ -2,6 +2,8 @@
 session_start();
 if(!$_SESSION['ok']) header("Location:login.php");
 
-echo "<h2>[KING]DevSupport Panel</h2>";
+echo "<h2>👑 [KING]DevSupport Panel</h2>";
+echo "<a href='create.php'>Create</a> | <a href='delete.php'>Delete</a><br><br>";
+
 echo nl2br(file_get_contents("/etc/autopanel/data/expiry.db"));
 ?>
